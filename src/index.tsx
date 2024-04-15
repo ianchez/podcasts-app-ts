d6d7bf85a4e4cb1d40d979ac0c3aeac6969096b0
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-15 10:59:49 +0200

    chore: renamed App to HomeScreen, setting screens folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import HomeScreen from './screens/HomeScreen';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HomeScreen />
   </React.StrictMode>
 );
 
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,18 @@
+const HomeScreen = () => {
+  return (
+    <div className="screen">
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </div>
+  );
+};
+
+export default HomeScreen;
