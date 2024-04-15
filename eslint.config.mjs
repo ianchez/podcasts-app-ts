e4c88b3897600ce2829c046f44418d18a932f06d
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-15 12:22:08 +0200

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,38 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+/* eslint-disable @typescript-eslint/naming-convention */
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: tseslint.configs.recommended })
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("standard-with-typescript"),
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
-];
+  ...compat.extends('standard-with-typescript'),
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+          tsx: true
+        }
+      },
+      globals: globals.browser
+    },
+    ...pluginReactConfig,
+
+    rules: {
+      // Add your own rules here
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'no-console': 'warn'
+    }
+  }
+]
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
 
 import {
   createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+  RouterProvider
+} from 'react-router-dom'
 
 // screens
-import NotFoundScreen from './screens/NotFoundScreen';
-import HomeScreen from './screens/HomeScreen';
+import NotFoundScreen from './screens/NotFoundScreen'
+import HomeScreen from './screens/HomeScreen'
 
 // TODO: Move this to a separate file
 const routes = [
   {
     path: '/',
-    element: <HomeScreen />,
+    element: <HomeScreen />
   },
   {
     path: '*',
-    element: <NotFoundScreen />,
-  },
-];
+    element: <NotFoundScreen />
+  }
+]
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes)
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root')
+
+if (rootElement === null) {
+  throw new Error('Could not find root element')
+}
+
+const root = ReactDOM.createRoot(rootElement)
 
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
